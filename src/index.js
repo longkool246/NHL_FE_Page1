64306e3658649a314696e83c9abf78755af95c0a
Nguyen Hai Long <longnh@timebird.org>
2023-07-26 12:00:31 +0700

    responsive-responsive3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+//Hamburger_Menu
 const headerBars = document.querySelector('.header__bars')
 const overlay = document.querySelector('.header__overlay')
 const navigation = document.querySelector('.navigation')
+const scrollHeader = document.querySelector('.header')
 
 headerBars.addEventListener('click', () => {
   overlay.classList.toggle('show-overlay')
@@ -11,3 +13,13 @@ overlay.addEventListener('click', () => {
   overlay.classList.remove('show-overlay')
   navigation.classList.remove('show-menu')
 })
+
+// Sticky_Menu
+window.addEventListener("scroll", function() {
+  var header = document.querySelector(".header");
+  if (window.scrollY > 0) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
